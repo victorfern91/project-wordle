b6a09e0a4d13ae7e566157a0ae70ff16353628e9
Victor Fernandes <victor.fernandes@rows.com>
2023-09-22 01:25:28 +0100

    Solve exercice 1

---
+++ src/components/Game/GuessInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('')
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+    console.log({ guess })
+
+    setGuess('');
+  }
+
+  function handleInputChange(evt) {
+    setGuess(evt.target.value.toUpperCase());
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        autoFocus
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="Must be a five letter word"
+        onChange={handleInputChange}/>
+    </form>
+  );
+}
+
+export default GuessInput;
